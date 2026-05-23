c42f5fdfedac1a010883d1d15e193c5b3e8fd6b0
Matias Villafañe <matutevm1@gmail.com>
2026-05-22 21:25:36 -0300

    feat: Caso List

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +10,31 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    private List<Alumno> _alumnos = new List<Alumno>();
+
+    public void Agregar(Alumno alumno)
+    {
+        _alumnos.Add(alumno);
+    }
+
+    public List<Alumno> ObtenerLista()
+    {
+        return _alumnos;
+    }
+
+    public Alumno? BuscarPorNombre(string nombre)
+    {
+        return _alumnos.FirstOrDefault(a => a.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public void Eliminar(Alumno alumno)
+    {
+        _alumnos.Remove(alumno);
+    }
+
+    public void EliminarEnPosicion(int posicion)
+    {
+        if (posicion >= 0 && posicion < _alumnos.Count)
+            _alumnos.RemoveAt(posicion);
+    }
 }
